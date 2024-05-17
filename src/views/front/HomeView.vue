31352a3aa7ec89da0773d35651602cdfed843fcb
tkooooo123 <tkooooo123@gmail.com>
2024-05-17 09:16:07 +0800

    新增 Layout 元件

---
+++ src/views/front/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="mt">
+    <h1>首頁</h1>
+
+  </main>
+</template>
+<style lang="scss" scoped>
+.mt {
+  padding-top: 72px;
+}
+</style>
---
+++ src/views/front/LayoutView.vue
@@ -0,0 +1,15 @@
+<template>
+    <NavbarComponent />
+    <RouterView />
+</template>
+
+<script>
+import NavbarComponent from '../../components/layout/NavbarComponent.vue'
+
+export default {
+    components: {
+        NavbarComponent,
+    }
+}
+</script>
+
