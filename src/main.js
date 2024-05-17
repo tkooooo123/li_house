dbb040e7477538c4dfb4d392775e94ee46b2ef87
tkooooo123 <tkooooo123@gmail.com>
2024-05-17 09:13:41 +0800

    安裝 SASS 與  Bootstrap 套件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const app = createApp(App)
 
