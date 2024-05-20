b8feaa8e8972a3e7bc9c670a709411d0d1d5222d
tkooooo123 <tkooooo123@gmail.com>
2024-05-21 01:12:26 +0800

    修正連結路徑

--- src/components/front/home/HomeLatest.vue
+++ src/components/front/home/HomeLatest.vue
@@ -3,7 +3,7 @@
         <h2 class="fs-2 fw-bold text-center text-primary">新品登場</h2>
         <ul class="p-0 row g-3">
             <li v-for="item in productList" :key="item.id" class="col-lg-3 col-sm-6">
-                <RouterLink :to="{ path: product, params: { productId: item.id}}">
+                <RouterLink :to="{path: `product/${item.id}`}">
                     <div class="product-card border border-primary rounded-2 h-100 d-flex flex-column">
                     <img class="product-img" :src="item.imageUrl" :alt="item.title">
                     <div class="d-flex flex-column flex-grow-1 p-2">
