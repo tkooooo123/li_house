9f2006a4d610164253a7f07c5ff0aa25d4a00adb
tkooooo123 <tkooooo123@gmail.com>
2024-05-21 09:35:57 +0800

    修正複製優惠碼功能

--- src/components/front/home/HomeActivity.vue
+++ src/components/front/home/HomeActivity.vue
@@ -1,34 +1,47 @@
 <template>
-    <div class="container pt-5">
-        <h2 class="text-primary fw-bold text-center">熱門活動</h2>
-        <div class="row">
-          <div class="col-md-6 text-center">
-            <div class="activity text-white mt-3">
-              <div class="activity-content">
-                <p class="fw-bold fs-3"><i class="bi bi-calendar3 me-2"></i>期間限定</p>
-                <h3 class="fw-bold pt-3">活動名稱：歡慶開幕</h3>
-                <p class="fw-bold fs-4">舉辦期間：即日起 ~ 8月31日</p>
-                <p class="fw-bold fs-4">折扣優惠：全站商品享88折</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 pt-3">
-            <div class="d-flex flex-column justify-content-center h-100">
-              <div className="ticket  mx-auto text-center  py-3 fs-5">
-                <span className="ticket-text fs-4">優惠碼</span>
-                <span className="ticket-dash  mx-2"></span>
-                <span className="ticket-code fs-4" ref={copyRef}>PET888</span>
-              </div>
-              <div class="text-center pt-3">
-                <button type="button"
-                  class="btn btn-primary rounded-0 p-3 mt-3 text-white fw-bold rounded-pill">複製優惠碼</button>
-              </div>
-            </div>
+  <div class="container pt-5">
+    <h2 class="text-primary fw-bold text-center">熱門活動</h2>
+    <div class="row">
+      <div class="col-md-6 text-center">
+        <div class="activity text-white mt-3">
+          <div class="activity-content">
+            <p class="fw-bold fs-3"><i class="bi bi-calendar3 me-2"></i>期間限定</p>
+            <h3 class="fw-bold pt-3">活動名稱：歡慶開幕</h3>
+            <p class="fw-bold fs-4">舉辦期間：即日起 ~ 8月31日</p>
+            <p class="fw-bold fs-4">折扣優惠：全站商品享88折</p>
           </div>
         </div>
       </div>
-</template>
+      <div class="col-md-6 pt-3">
+        <div class="d-flex flex-column justify-content-center h-100">
+          <div className="ticket  mx-auto text-center  py-3 fs-5">
+            <span className="ticket-text fs-4">優惠碼</span>
+            <span className="ticket-dash  mx-2"></span>
+            <span className="ticket-code fs-4">PET888</span>
+          </div>
+          <div class="text-center pt-3">
+            <button type="button" @click="copy('PET888')"
+              class="btn btn-primary rounded-0 p-3 mt-3 text-white fw-bold rounded-pill"
+              :class="{disabled : text}"
+              >
+              <i class="bi"
+              :class="{'bi-calendar' : !text, 'bi-calendar-check' : text}"           
+              ></i>
+              {{ text ? '複製成功' : '複製優惠碼' }}
 
+            </button>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+<script setup>
+import { useClipboard } from '@vueuse/core'
+const { text, copy } = useClipboard()
+console.log(text)
+</script>
 <style lang="scss" scoped>
 @import 'bootstrap/scss/functions';
 @import '@/assets/helpers/variables';
@@ -99,4 +112,4 @@
   border-left: 1px solid black;
 
 }
-</style>   
+</style>
